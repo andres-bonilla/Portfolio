14a348f7e281ee01a5559cf123ae743c2ce14243
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-08-21 12:48:17 -0300

    Project hover-effect and minor changes on header

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,11 +4,20 @@ import { ProjectItem } from "../commons/ProjectItem";
 export const Projects = () => {
   return (
     <section id="projects">
-      <h2>Projects</h2>
+      <h2 id="project-title">
+        <span className="project-title-space"></span>
+        <span id="project-title-text">
+          <span id="project-title-text-left">My</span>
+          <span id="project-title-text-right">Projects</span>
+        </span>
+        <span className="project-title-space"></span>
+      </h2>
       {/*<p>
         If you have any advice for my projects, I'll be eager to receive your
         feedback!
-      </p>*/}
+      </p>
+     <h2>Projects</h2>
+       */}
       <ul className="project-grid">
         <li className="project-item">
           <ProjectItem />
