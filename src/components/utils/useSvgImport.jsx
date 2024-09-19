6273e78c6e1af280833e874b4bdb12aeed653cbf
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-09-18 21:27:35 -0300

    Test import 4

--- src/components/utils/useSvgImport.jsx
+++ src/components/utils/useSvgImport.jsx
@@ -9,9 +9,7 @@ export const useSvgImport = (logoFile) => {
 
     const importSvgLogo = async () => {
       try {
-        logoRef.current = (
-          await import(`/tech-logos/t-react.svg`)
-        ).ReactComponent;
+        logoRef.current = (await import(`/t-react.svg`)).ReactComponent;
       } finally {
         setIsLoading(false);
       }
