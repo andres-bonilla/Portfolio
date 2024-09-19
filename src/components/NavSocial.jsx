fbe52375ecbfaa84be8a7aa4c309e6c5b0e887f3
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-09-19 03:51:52 -0300

    Minor Fix on NavSocial

--- src/components/NavSocial.jsx
+++ src/components/NavSocial.jsx
@@ -10,28 +10,25 @@ import linkedin from "./../assets/social-logos/linkedin.svg";
 export const NavSocial = () => {
   const mapSocialLinks = (list) => {
     const logoPaths = { github, gmail, linkedin };
+
     return list.map((item, i) => {
+      const url = { "--url": `url("${logoPaths[item.file]}")` };
+      const classes =
+        "social-link cancel-link-style spin-hover spin-down tooltip-hover";
+
       return (
         <li key={i} className="spin-hover">
           {i === 0 ? (
-            <div
-              style={{ "--url": `url("${logoPaths[item.file]}")` }}
-              className="social-link cancel-link-style spin-hover spin-down tooltip-hover"
-            >
+            <div className={classes}>
               <EmailTip email={item.link} />
 
-              <span className="social-logo spin-down" />
+              <span className="social-logo spin-down" style={url} />
             </div>
           ) : (
-            <a
-              href={item.link}
-              target="_blank"
-              style={{ "--url": `url("${logoPaths[item.file]}")` }}
-              className="social-link cancel-link-style spin-hover spin-down tooltip-hover"
-            >
+            <a href={item.link} target="_blank" className={classes}>
               <span className="social-tip tooltip">{item.network}</span>
 
-              <span className="social-logo spin-down" />
+              <span className="social-logo spin-down" style={url} />
             </a>
           )}
         </li>
