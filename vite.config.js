d39e411b938645c40bfb8e21540ca20d413a2e4c
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-09-18 22:42:42 -0300

    Test import 6

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import importDynamicModule from "vite-plugin-dynamic-import-module";
+import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 
 //  https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), importDynamicModule()],
+  plugins: [react(), svgr(), dynamicImportVars()],
 });
