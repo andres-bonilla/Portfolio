20f8c1bc9e626d688a85f3eaa65e557da78c0039
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-10-03 18:03:20 -0300

    Colors adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer id="foot">
-      <p>Andrés Bonilla © 2024 - All Rights Reserved</p>
+      <p>
+        <span>Andrés Bonilla © 2024</span>
+        <span id="dash"> - </span>
+        <span>All Rights Reserved</span>
+      </p>
     </footer>
   );
 };
